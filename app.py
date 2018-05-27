0118b66b8f2efe8b8c8caf44144ff2858ca280e8
Aaron Johnson <aaronmjohnson29@gmail.com>
2018-05-27 15:55:28 -0500

    add sound effect before kick

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import discord
+import discord, asyncio
 import secret
 
 client = discord.Client()
@@ -20,8 +20,19 @@ async def on_message(message):
             return
         try:
             channel = await client.create_channel(server, "kicked", type=discord.ChannelType.voice)
-        
             await client.move_member(member, channel)
+            voice = await client.join_voice_channel(channel)
+            player = voice.create_ffmpeg_player('sound.mp3')
+            player.start()
+            
+            count = 0
+            length = 3
+
+            while count < length:
+                await asyncio.sleep(1)
+                count += 1
+            player.stop()
+            await voice.disconnect()
             await client.delete_channel(channel)
             await client.send_message(message.channel, "Kicked " + member.name)
         except discord.Forbidden:
